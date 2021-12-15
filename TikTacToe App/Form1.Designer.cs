c2972aa8a05b0dc980a9e295a5392f03749f7a77
Senon Silvestre <42272961+senjaymac@users.noreply.github.com>
2021-12-15 14:50:39 +0800

    Add files via upload
    Revised and Final TikTacToe w/ pop

---
+++ TikTacToe App/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace TikTacToe_App
+{
+    partial class TikTakToeF1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LabelTikTakToe = new System.Windows.Forms.Label();
+            this.Container = new System.Windows.Forms.GroupBox();
+            this.BtnB9 = new System.Windows.Forms.Button();
+            this.BtnB8 = new System.Windows.Forms.Button();
+            this.BtnB7 = new System.Windows.Forms.Button();
+            this.BtnB6 = new System.Windows.Forms.Button();
+            this.BtnB5 = new System.Windows.Forms.Button();
+            this.BtnB4 = new System.Windows.Forms.Button();
+            this.BtnB3 = new System.Windows.Forms.Button();
+            this.BtnB2 = new System.Windows.Forms.Button();
+            this.BtnB1 = new System.Windows.Forms.Button();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.howToPlayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.Container.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LabelTikTakToe
+            // 
+            this.LabelTikTakToe.AutoSize = true;
+            this.LabelTikTakToe.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelTikTakToe.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.LabelTikTakToe.Location = new System.Drawing.Point(163, 35);
+            this.LabelTikTakToe.Name = "LabelTikTakToe";
+            this.LabelTikTakToe.Size = new System.Drawing.Size(257, 51);
+            this.LabelTikTakToe.TabIndex = 0;
+            this.LabelTikTakToe.Text = "Tik-Tak-Toe";
+            this.LabelTikTakToe.Click += new System.EventHandler(this.LabelTikTakToe_Click);
+            // 
+            // Container
+            // 
+            this.Container.Controls.Add(this.BtnB9);
+            this.Container.Controls.Add(this.BtnB8);
+            this.Container.Controls.Add(this.BtnB7);
+            this.Container.Controls.Add(this.BtnB6);
+            this.Container.Controls.Add(this.BtnB5);
+            this.Container.Controls.Add(this.BtnB4);
+            this.Container.Controls.Add(this.BtnB3);
+            this.Container.Controls.Add(this.BtnB2);
+            this.Container.Controls.Add(this.BtnB1);
+            this.Container.Location = new System.Drawing.Point(12, 89);
+            this.Container.Name = "Container";
+            this.Container.Size = new System.Drawing.Size(542, 451);
+            this.Container.TabIndex = 1;
+            this.Container.TabStop = false;
+            // 
+            // BtnB9
+            // 
+            this.BtnB9.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.BtnB9.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnB9.Location = new System.Drawing.Point(386, 320);
+            this.BtnB9.Name = "BtnB9";
+            this.BtnB9.Size = new System.Drawing.Size(150, 125);
+            this.BtnB9.TabIndex = 10;
+            this.BtnB9.UseVisualStyleBackColor = false;
+            this.BtnB9.Click += new System.EventHandler(this.BtnB9_Click);
+            // 
+            // BtnB8
+            // 
+            this.BtnB8.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.BtnB8.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnB8.Location = new System.Drawing.Point(203, 320);
+            this.BtnB8.Name = "BtnB8";
+            this.BtnB8.Size = new System.Drawing.Size(150, 125);
+            this.BtnB8.TabIndex = 9;
+            this.BtnB8.UseVisualStyleBackColor = false;
+            this.BtnB8.Click += new System.EventHandler(this.BtnB8_Click);
+            // 
+            // BtnB7
+            // 
+            this.BtnB7.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.BtnB7.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnB7.Location = new System.Drawing.Point(6, 320);
+            this.BtnB7.Name = "BtnB7";
+            this.BtnB7.Size = new System.Drawing.Size(150, 125);
+            this.BtnB7.TabIndex = 8;
+            this.BtnB7.UseVisualStyleBackColor = false;
+            this.BtnB7.Click += new System.EventHandler(this.BtnB7_Click);
+            // 
+            // BtnB6
+            // 
+            this.BtnB6.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.BtnB6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnB6.Location = new System.Drawing.Point(386, 168);
+            this.BtnB6.Name = "BtnB6";
+            this.BtnB6.Size = new System.Drawing.Size(150, 125);
+            this.BtnB6.TabIndex = 7;
+            this.BtnB6.UseVisualStyleBackColor = false;
+            this.BtnB6.Click += new System.EventHandler(this.BtnB6_Click);
+            // 
+            // BtnB5
+            // 
+            this.BtnB5.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.BtnB5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnB5.Location = new System.Drawing.Point(203, 168);
+            this.BtnB5.Name = "BtnB5";
+            this.BtnB5.Size = new System.Drawing.Size(150, 125);
+            this.BtnB5.TabIndex = 6;
+            this.BtnB5.UseVisualStyleBackColor = false;
+            this.BtnB5.Click += new System.EventHandler(this.BtnB5_Click);
+            // 
+            // BtnB4
+            // 
+            this.BtnB4.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.BtnB4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnB4.Location = new System.Drawing.Point(6, 168);
+            this.BtnB4.Name = "BtnB4";
+            this.BtnB4.Size = new System.Drawing.Size(150, 125);
+            this.BtnB4.TabIndex = 5;
+            this.BtnB4.UseVisualStyleBackColor = false;
+            this.BtnB4.Click += new System.EventHandler(this.BtnB4_Click);
+            // 
+            // BtnB3
+            // 
+            this.BtnB3.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.BtnB3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnB3.Location = new System.Drawing.Point(386, 19);
+            this.BtnB3.Name = "BtnB3";
+            this.BtnB3.Size = new System.Drawing.Size(150, 125);
+            this.BtnB3.TabIndex = 4;
+            this.BtnB3.UseVisualStyleBackColor = false;
+            this.BtnB3.Click += new System.EventHandler(this.BtnB3_Click);
+            // 
+            // BtnB2
+            // 
+            this.BtnB2.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.BtnB2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnB2.Location = new System.Drawing.Point(203, 19);
+            this.BtnB2.Name = "BtnB2";
+            this.BtnB2.Size = new System.Drawing.Size(150, 125);
+            this.BtnB2.TabIndex = 3;
+            this.BtnB2.UseVisualStyleBackColor = false;
+            this.BtnB2.Click += new System.EventHandler(this.BtnB2_Click);
+            // 
+            // BtnB1
+            // 
+            this.BtnB1.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.BtnB1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnB1.Location = new System.Drawing.Point(6, 19);
+            this.BtnB1.Name = "BtnB1";
+            this.BtnB1.Size = new System.Drawing.Size(150, 125);
+            this.BtnB1.TabIndex = 2;
+            this.BtnB1.UseVisualStyleBackColor = false;
+            this.BtnB1.Click += new System.EventHandler(this.BtnB1_Click);
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // howToPlayToolStripMenuItem
+            // 
+            this.howToPlayToolStripMenuItem.Name = "howToPlayToolStripMenuItem";
+            this.howToPlayToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.howToPlayToolStripMenuItem.Text = "How To Play";
+            this.howToPlayToolStripMenuItem.Click += new System.EventHandler(this.howToPlayToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.howToPlayToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(570, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // TikTakToeF1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(570, 552);
+            this.Controls.Add(this.Container);
+            this.Controls.Add(this.LabelTikTakToe);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "TikTakToeF1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tik-Tac-Toe (X vs. O)";
+            this.Container.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelTikTakToe;
+        private System.Windows.Forms.GroupBox Container;
+        private System.Windows.Forms.Button BtnB9;
+        private System.Windows.Forms.Button BtnB8;
+        private System.Windows.Forms.Button BtnB7;
+        private System.Windows.Forms.Button BtnB6;
+        private System.Windows.Forms.Button BtnB5;
+        private System.Windows.Forms.Button BtnB4;
+        private System.Windows.Forms.Button BtnB3;
+        private System.Windows.Forms.Button BtnB2;
+        private System.Windows.Forms.Button BtnB1;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem howToPlayToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
---
+++ TikTacToe App/Form1.cs
@@ -0,0 +1,425 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TikTacToe_App
+{
+    public partial class TikTakToeF1 : Form
+    {
+        bool Player_Turn = true;
+        SoundPlayer ClickSoundFxPop = new SoundPlayer(@"C:\Users\Senon\Documents\Repositories\TikTacToe App\TikTacToe App\Properties\PopFx.wav");
+
+        public TikTakToeF1()
+        {
+            InitializeComponent();
+        }
+
+        private void LabelTikTakToe_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void howToPlayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("1.The game is played on a grid that's 3 squares by 3 squares. \n2.You are X, your friend(or the computer in this case) is O.Players take turns putting their marks in empty squares. \n3.The first player to get 3 of her marks in a row(up, down, across, or diagonally) is the winner. \n4.When all 9 squares are full, the game is over.If no player has 3 marks in a row, the game ends in a tie.");
+        }
+
+        public void BtnFalse()
+        {
+            BtnB1.Enabled = false;
+            BtnB2.Enabled = false;
+            BtnB3.Enabled = false;
+            BtnB4.Enabled = false;
+            BtnB5.Enabled = false;
+            BtnB6.Enabled = false;
+            BtnB7.Enabled = false;
+            BtnB8.Enabled = false;
+            BtnB9.Enabled = false;
+
+        }
+
+        public void Checker()
+        {
+            //For X, Horizontal//
+            if (BtnB1.Text == "X" && BtnB2.Text == "X" && BtnB3.Text == "X")
+            {
+                //Verifier//
+                BtnB1.BackColor = System.Drawing.Color.Gray;
+                BtnB2.BackColor = System.Drawing.Color.Gray;
+                BtnB3.BackColor = System.Drawing.Color.Gray;
+                MessageBox.Show("X Wins!");
+                BtnFalse();
+            }
+
+            if (BtnB4.Text == "X" && BtnB5.Text == "X" && BtnB6.Text == "X")
+            {
+                //Verifier//
+                BtnB4.BackColor = System.Drawing.Color.Gray;
+                BtnB5.BackColor = System.Drawing.Color.Gray;
+                BtnB6.BackColor = System.Drawing.Color.Gray;
+                MessageBox.Show("X Wins!");
+                BtnFalse();
+            }
+
+            if (BtnB7.Text == "X" && BtnB8.Text == "X" && BtnB9.Text == "X")
+            {
+                //Verifier//
+                BtnB7.BackColor = System.Drawing.Color.Gray;
+                BtnB8.BackColor = System.Drawing.Color.Gray;
+                BtnB9.BackColor = System.Drawing.Color.Gray;
+                MessageBox.Show("X Wins!");
+                BtnFalse();
+            }
+
+            //For X Vertical//
+
+            if (BtnB1.Text == "X" && BtnB4.Text == "X" && BtnB7.Text == "X")
+            {
+                //Verifier//
+                BtnB1.BackColor = System.Drawing.Color.Gray;
+                BtnB4.BackColor = System.Drawing.Color.Gray;
+                BtnB7.BackColor = System.Drawing.Color.Gray;
+                MessageBox.Show("X Wins!");
+                BtnFalse();
+            }
+
+            if (BtnB2.Text == "X" && BtnB5.Text == "X" && BtnB8.Text == "X")
+            {
+                //Verifier//
+                BtnB2.BackColor = System.Drawing.Color.Gray;
+                BtnB5.BackColor = System.Drawing.Color.Gray;
+                BtnB8.BackColor = System.Drawing.Color.Gray;
+                MessageBox.Show("X Wins!");
+                BtnFalse();
+            }
+
+            if (BtnB3.Text == "X" && BtnB6.Text == "X" && BtnB9.Text == "X")
+            {
+                //Verifier//
+                BtnB3.BackColor = System.Drawing.Color.Gray;
+                BtnB6.BackColor = System.Drawing.Color.Gray;
+                BtnB9.BackColor = System.Drawing.Color.Gray;
+                MessageBox.Show("X Wins!");
+                BtnFalse();
+            }
+
+            // For X Diagonal //
+            if (BtnB1.Text == "X" && BtnB5.Text == "X" && BtnB9.Text == "X")
+            {
+                //Verifier//
+                BtnB1.BackColor = System.Drawing.Color.Gray;
+                BtnB5.BackColor = System.Drawing.Color.Gray;
+                BtnB9.BackColor = System.Drawing.Color.Gray;
+                MessageBox.Show("X Wins!");
+                BtnFalse();
+            }
+
+            if (BtnB3.Text == "X" && BtnB5.Text == "X" && BtnB7.Text == "X")
+            {
+                //Verifier//
+                BtnB3.BackColor = System.Drawing.Color.Gray;
+                BtnB5.BackColor = System.Drawing.Color.Gray;
+                BtnB7.BackColor = System.Drawing.Color.Gray;
+                MessageBox.Show("X Wins!");
+                BtnFalse();
+            }
+
+
+
+
+
+                                      //For O, Horizontal//
+            if (BtnB1.Text == "O" && BtnB2.Text == "O" && BtnB3.Text == "O")
+            {
+                //Verifier//
+                BtnB1.BackColor = System.Drawing.Color.Gray;
+                BtnB2.BackColor = System.Drawing.Color.Gray;
+                BtnB3.BackColor = System.Drawing.Color.Gray;
+                MessageBox.Show("O Wins!");
+                BtnFalse();
+            }
+
+            if (BtnB4.Text == "O" && BtnB5.Text == "O" && BtnB6.Text == "O")
+            {
+                //Verifier//
+                BtnB4.BackColor = System.Drawing.Color.Gray;
+                BtnB5.BackColor = System.Drawing.Color.Gray;
+                BtnB6.BackColor = System.Drawing.Color.Gray;
+                MessageBox.Show("O Wins!");
+                BtnFalse();
+            }
+
+            if (BtnB7.Text == "O" && BtnB8.Text == "O" && BtnB9.Text == "O")
+            {
+                //Verifier//
+                BtnB7.BackColor = System.Drawing.Color.Gray;
+                BtnB8.BackColor = System.Drawing.Color.Gray;
+                BtnB9.BackColor = System.Drawing.Color.Gray;
+                MessageBox.Show("O Wins!");
+                BtnFalse();
+            }
+
+                                        //For O Vertical//
+
+            if (BtnB1.Text == "O" && BtnB4.Text == "O" && BtnB7.Text == "O")
+            {
+                //Verifier//
+                BtnB1.BackColor = System.Drawing.Color.Gray;
+                BtnB4.BackColor = System.Drawing.Color.Gray;
+                BtnB7.BackColor = System.Drawing.Color.Gray;
+                MessageBox.Show("O Wins!");
+                BtnFalse();
+            }
+
+            if (BtnB2.Text == "O" && BtnB5.Text == "O" && BtnB8.Text == "O")
+            {
+                //Verifier//
+                BtnB2.BackColor = System.Drawing.Color.Gray;
+                BtnB5.BackColor = System.Drawing.Color.Gray;
+                BtnB8.BackColor = System.Drawing.Color.Gray;
+                MessageBox.Show("O Wins!");
+                BtnFalse();
+            }
+
+            if (BtnB3.Text == "O" && BtnB6.Text == "O" && BtnB9.Text == "O")
+            {
+                //Verifier//
+                BtnB3.BackColor = System.Drawing.Color.Gray;
+                BtnB6.BackColor = System.Drawing.Color.Gray;
+                BtnB9.BackColor = System.Drawing.Color.Gray;
+                MessageBox.Show("O Wins!");
+                BtnFalse();
+            }
+
+                                      // For O Diagonal //
+            if (BtnB1.Text == "O" && BtnB5.Text == "O" && BtnB9.Text == "O")
+            {
+                //Verifier//
+                BtnB1.BackColor = System.Drawing.Color.Gray;
+                BtnB5.BackColor = System.Drawing.Color.Gray;
+                BtnB9.BackColor = System.Drawing.Color.Gray;
+                MessageBox.Show("O Wins!");
+                BtnFalse();
+            }
+
+            if (BtnB3.Text == "O" && BtnB5.Text == "O" && BtnB7.Text == "O")
+            {
+                //Verifier//
+                BtnB3.BackColor = System.Drawing.Color.Gray;
+                BtnB5.BackColor = System.Drawing.Color.Gray;
+                BtnB7.BackColor = System.Drawing.Color.Gray;
+                MessageBox.Show("O Wins!");
+                BtnFalse();
+            }
+
+        }
+
+        public void BtnB1_Click(object sender, EventArgs e)
+        {
+            ClickSoundFxPop.Play();
+            if (Player_Turn == false)
+            {
+                BtnB1.Text = "O";
+                Player_Turn = true;
+            }
+            else
+            {
+                BtnB1.Text = "X";
+                Player_Turn = false;
+            }
+            Checker();
+            BtnB1.Enabled = false;
+            
+        }
+
+
+        public void BtnB2_Click(object sender, EventArgs e)
+        {
+            ClickSoundFxPop.Play();
+            if (Player_Turn == false)
+            {
+                BtnB2.Text = "O";
+                Player_Turn = true;
+            }
+            else
+            {
+                BtnB2.Text = "X";
+                Player_Turn = false;
+            }
+            Checker();
+            BtnB2.Enabled = false;
+            
+        }
+
+        public void BtnB3_Click(object sender, EventArgs e)
+        {
+            ClickSoundFxPop.Play();
+            if (Player_Turn == false)
+            {
+                BtnB3.Text = "O";
+                Player_Turn = true;
+            }
+            else
+            {
+                BtnB3.Text = "X";
+                Player_Turn = false;
+            }
+            Checker();
+            BtnB3.Enabled = false;
+            
+        }
+
+        public void BtnB4_Click(object sender, EventArgs e)
+        {
+            ClickSoundFxPop.Play();
+            if (Player_Turn == false)
+            {
+                BtnB4.Text = "O";
+                Player_Turn = true;
+            }
+            else
+            {
+                BtnB4.Text = "X";
+                Player_Turn = false;
+            }
+            Checker();
+            BtnB4.Enabled = false;
+            
+        }
+
+
+        public void BtnB5_Click(object sender, EventArgs e)
+        {
+            ClickSoundFxPop.Play();
+            if (Player_Turn == false)
+            {
+                BtnB5.Text = "O";
+                Player_Turn = true;
+            }
+            else
+            {
+                BtnB5.Text = "X";
+                Player_Turn = false;
+            }
+            Checker();
+            BtnB5.Enabled = false;
+            
+        }
+        public void BtnB6_Click(object sender, EventArgs e)
+        {
+            ClickSoundFxPop.Play();
+            if (Player_Turn == false)
+            {
+                BtnB6.Text = "O";
+                Player_Turn = true;
+            }
+            else
+            {
+                BtnB6.Text = "X";
+                Player_Turn = false;
+            }
+            Checker();
+            BtnB6.Enabled = false;
+            
+        }
+
+        public void BtnB7_Click(object sender, EventArgs e)
+        {
+            ClickSoundFxPop.Play();
+            if (Player_Turn == false)
+            {
+                BtnB7.Text = "O";
+                Player_Turn = true;
+            }
+            else
+            {
+                BtnB7.Text = "X";
+                Player_Turn = false;
+            }
+            Checker();
+            BtnB7.Enabled = false;
+            
+        }
+
+        public void BtnB8_Click(object sender, EventArgs e)
+        {
+            ClickSoundFxPop.Play();
+            if (Player_Turn == false)
+            {
+                BtnB8.Text = "O";
+                Player_Turn = true;
+            }
+            else
+            {
+                BtnB8.Text = "X";
+                Player_Turn = false;
+            }
+            Checker();
+            BtnB8.Enabled = false;
+            
+        }
+        public void BtnB9_Click(object sender, EventArgs e)
+        {
+            ClickSoundFxPop.Play();
+            if (Player_Turn == false)
+            {
+                BtnB9.Text = "O";
+                Player_Turn = true;
+            }
+            else
+            {
+                BtnB9.Text = "X";
+                Player_Turn = false;
+            }
+            Checker();
+            BtnB9.Enabled = false;
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            {
+                BtnB1.Enabled = true;
+                BtnB2.Enabled = true;
+                BtnB3.Enabled = true;
+                BtnB4.Enabled = true;
+                BtnB5.Enabled = true;
+                BtnB6.Enabled = true;
+                BtnB7.Enabled = true;
+                BtnB8.Enabled = true;
+                BtnB9.Enabled = true;
+
+                BtnB1.Text = "";
+                BtnB2.Text = "";
+                BtnB3.Text = "";
+                BtnB4.Text = "";
+                BtnB5.Text = "";
+                BtnB6.Text = "";
+                BtnB7.Text = "";
+                BtnB8.Text = "";
+                BtnB9.Text = "";
+
+                BtnB1.BackColor = Color.DarkTurquoise;
+                BtnB2.BackColor = Color.DarkTurquoise;
+                BtnB3.BackColor = Color.DarkTurquoise;
+                BtnB4.BackColor = Color.DarkTurquoise;
+                BtnB5.BackColor = Color.DarkTurquoise;
+                BtnB6.BackColor = Color.DarkTurquoise;
+                BtnB7.BackColor = Color.DarkTurquoise;
+                BtnB8.BackColor = Color.DarkTurquoise;
+                BtnB9.BackColor = Color.DarkTurquoise;
+            }
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
